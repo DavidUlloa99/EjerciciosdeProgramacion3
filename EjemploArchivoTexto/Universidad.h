984fb5b952d2029420bfc7df05250a2d81ef2e1f
DavidUlloa99 <37524825+DavidUlloa99@users.noreply.github.com>
2021-06-17 22:41:02 -0600

    Actividad 5

---
+++ EjemploArchivoTexto/Universidad.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef UNIVERSIDAD_H
+#define UNIVERSIDAD_H
+
+class Universidad
+{
+public:
+	static void agregarAlumnos();
+	static void consultarAlumnos();
+	static void agregarMaterias();
+	static void consultarMaterias();
+	static void promedioNotas();
+
+	static bool existeAlumno(int);
+	static bool existeMateria(int);
+
+	static void agregarNotas();
+};
+
+
+#endif // !UNIVERSIDAD_H
---
+++ EjemploArchivoTexto/main.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include "Universidad.h"
+
+int main()
+{
+	//Universidad::agregarAlumnos();
+	//Universidad::consultarAlumnos();
+
+	//Universidad::agregarMaterias();
+	//Universidad::consultarMaterias();
+
+	Universidad::agregarNotas();
+
+
+	_getch();
+}
